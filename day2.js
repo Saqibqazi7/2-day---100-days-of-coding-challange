dff17a1344d1008aecd1245e788068cd93f6e2e0
Saqibqazi7 <160391573+Saqibqazi7@users.noreply.github.com>
2024-03-19 01:15:27 +0500

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,9 @@
+//let person_name = "Imam Shafi(R.A)"
+//console.log(person_name, 'once said: knowledge is that which benefits not that which is memorized')
+//let person_name: string = 'Hz Umar bin al-khatab(R.A)';
+//let message = 'patience is a pillar of faith';
+//console.log({person_name: person_name}, 'once said', {message});
+//Stripping Names;
+var person_name = "\t\n Saqib Qazi";
+console.log(person_name);
+console.log(person_name.trim());
